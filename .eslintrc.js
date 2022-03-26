b9529beda1704d24916e7ac5d00913f46e81a968
Thiago Henrique <thiagohenrique776@gmail.com>
2022-03-26 19:50:25 -0300

    Joins e tratamento de erros

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,6 @@ module.exports = {
     'consistent-return': 'off',
     'no-promise-executor-return': 'off',
     camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
